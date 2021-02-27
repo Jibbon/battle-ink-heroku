9d382e5283b6a3216e0d291a470ea13eaaf00247
Jibbon <joseph@jibbon.com>
2021-02-28 09:46:25 +1100

    beginnings

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
-const app = require('express')();
+const express = require("express");
+const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
-});
+// Static files
+app.use(express.static("public"));
+
+
+var tracks = [
+  {'id':'cathedral', 'class':'diegetic', 'name':'Cathedral Ambience', 'file':'cathedral.mp3', 'column':'columnone', 'position':1 },
+  {'id':'farewell', 'class':'diegetic', 'name':'Farewell to a Friend', 'file':'farewelltoafriend.mp3', 'column':'columnone', 'position':2 },
+  {'id':'theglory', 'class':'diegetic', 'name':'The Glory', 'file':'theglory.mp3', 'column':'columnone', 'position':3 },
+  {'id':'darkcello', 'class':'diegetic', 'name':'Dark Cello One', 'file':'cellofear01.mp3', 'column':'columnone', 'position':4 },
+  ];
+
 
 io.on('connection', (socket) => {
   socket.on('chat message', msg => {
