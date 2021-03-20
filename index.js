f0c458f4a38226f37c5c7959adeab6a6ba0eb8b6
Jibbon <joseph@jibbon.com>
2021-03-20 21:53:24 +1100

    reverting

--- index.js
+++ index.js
@@ -1,21 +1,11 @@
 const express = require("express");
-const socket = require("socket.io");
-const siofu = require("socketio-file-upload");
-const fs = require('fs');
-
-// App setup
-const PORT = 3000;
 const app = express();
-const server = app.use(siofu.router).listen(PORT, function () {
-  console.log(`Listening on port ${PORT}`);
-  console.log(`http://localhost:${PORT}`);
-});
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const port = process.env.PORT || 3000;
 
 // Static files
 app.use(express.static("public"));
-
-// Socket setup
-const io = socket(server);
 
 
 var tracks = [
@@ -26,14 +16,6 @@ var tracks = [
 
   ];
 
-  var library = [
-
-    {'id':'farewell', 'file':'farewelltoafriend.mp3', "gain":0.05, "icon":"music" },
-    {'id':'swamp', 'file':'swamp.mp3', "gain":0.05, "icon":"water" },
-    {'id':'drums', 'file':'kododrums.mp3', "gain":0.05, "icon":"shield" }
-  
-    ];
-
 
 io.on('connection', (socket) => {
   socket.on('chat message', msg => {
@@ -41,8 +23,6 @@ io.on('connection', (socket) => {
   });
 
   socket.on('gettracks', (data) => { io.emit('sendtracks', tracks); });
-  socket.on('getlibrary', (data) => { io.emit('sendlibrary', library); });
-
   socket.on("volume", (data) => 
     { 
     var $index = tracks.findIndex(x => x.id === data.name);
@@ -51,45 +31,16 @@ io.on('connection', (socket) => {
     });
   socket.on("pan", (data) => { io.emit("changepan", data); });
   socket.on("syncit", (data) => { io.emit("sync", data); });
-  // add sound
   socket.on("seedsound", (data) => 
     { 
     $new = {'id':data.name, 'file':data.file, "gain":0, "icon":"music" };
     tracks.push($new);
     io.emit("newsound"); 
     });
-    // remove sound
-    socket.on("removesound", (name) => 
-    { 
-    var $index = tracks.findIndex(x => x.id === name);
-    tracks.splice($index, 1);
-    io.emit("soundscrubbed", name); 
-    });
-    // UPLOADING STUFF
-    
-    var uploader = new siofu();
-    uploader.dir = "public/audio";
-    uploader.listen(socket);
-
-    uploader.on("saved", function(event)
-      {
-      var filename = event.file.name;
-      var extension = filename.split('.').pop();
-      var name = filename.replace(/\.[^/.]+$/, "");
-      var d = new Date();
-      var n = d.getTime();
-      
-      $newtrack =  {'id':name, 'file':n+'.'+extension, 'gain':0.05, 'icon':'water' };
-      library.push($newtrack);
-
-      fs.rename(event.file.pathName, 'public/audio/'+n+'.'+extension, function(err) 
-        {
-        if ( err ) console.log('ERROR: ' + err);
-        else io.emit("sounduploaded", $newtrack);
-      });
-      });
 
 
 });
 
-
+http.listen(port, () => {
+  console.log(`Socket.IO server running at http://localhost:${port}/`);
+});
