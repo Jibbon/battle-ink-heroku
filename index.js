722a59692be91652f4ce68c4e940302275a50d66
Jibbon <joseph@jibbon.com>
2021-03-26 18:06:36 +1100

    larger library

--- index.js
+++ index.js
@@ -12,12 +12,19 @@ var library;
 
 var tracks = [];
 
+var presets = [];
+
 
 fs.readFile('library.json', (err, data) => {
   if (err) throw err;
   library = JSON.parse(data);
 });
 
+
+fs.readFile('presets.json', (err, data) => {
+  if (err) throw err;
+  presets = JSON.parse(data);
+});
     var background = "https://cdna.artstation.com/p/assets/images/images/020/186/524/large/miloe-cute-258-final-8-mb-pngg.jpg?1566760419";
 
 
@@ -30,6 +37,7 @@ io.on('connection', (socket) => {
 
   socket.on('gettracks', (data) => { io.emit('sendtracks', tracks); });
   socket.on('getlibrary', (data) => { io.emit('sendlibrary', library); });
+  socket.on('getpresets', (data) => { io.emit('sendpresets', presets); });
   socket.on('getbackground', (data) => { io.emit('feedbackground', background); });
   // clock function
   socket.on("tick", (data) => { io.emit("tock"); });
@@ -57,6 +65,13 @@ io.on('connection', (socket) => {
   socket.on("syncit", (data) => { io.emit("sync", data); });
   // add sound
   socket.on("seedsound", (data) => 
+    { 
+    $new = {'id':data.name, 'file':data.file, "gain":data.gain, 'pan':data.pan, 'loop':data.loop, "icon":data.icon };
+    tracks.push($new);
+    io.emit("newsound"); 
+    });
+    // add preset sound
+    socket.on("seedpreset", (data) => 
     { 
     $new = {'id':data.name, 'file':data.file, "gain":0, "icon":data.icon };
     tracks.push($new);
